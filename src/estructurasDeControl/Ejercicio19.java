0268989ddb66482ac47ae46666c24f6a004a06af
anaijg <ana.jimenezgomez@educa.madrid.org>
2021-10-18 20:38:33 +0200

    Enunciados 19,20,21

---
+++ src/estructurasDeControl/Ejercicio19.java
@@ -0,0 +1,16 @@
+package estructurasDeControl;
+
+public class Ejercicio19 {
+    public static void main(String[] args) {
+        /*
+        19. Realizar un programa que pida un número entero por teclado y visualice el dia de la
+semana correspondiente.
+         */
+
+        // Pido un número entero por teclado (1-7)
+
+        // Visualizo el día de la semana
+        // si 1 --> "Lunes"
+        // si 2 --> "Martes"
+    }
+}
---
+++ src/estructurasDeControl/Ejercicio20.java
@@ -0,0 +1,24 @@
+package estructurasDeControl;
+
+public class Ejercicio20 {
+    public static void main(String[] args) {
+        /*
+        20. Escribe un programa que lea dos números enteros positivos, y pida la operación que
+quiere realizar con estos (+, -, *, /). El programa debe realizar la operación pedida y
+visualizar el resultado. En caso de que el símbolo introducido no sea correcto,
+visualizar: operación incorrecta.
+         */
+
+        // pido un número entero positivo
+
+        // pido otro número entero positivo
+
+        // operación que quiere realizar con estos (Menú: +, -, *, /) <- los recogemos como char
+
+        // El programa debe realizar la operación pedida y visualizar el resultado. En caso de que el símbolo introducido no sea correcto,
+        //visualizar: operación incorrecta.
+
+
+
+    }
+}
