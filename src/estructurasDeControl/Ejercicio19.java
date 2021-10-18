07b181eed9f936aadd8501ad0906455ad2d0c697
anaijg <ana.jimenezgomez@educa.madrid.org>
2021-10-18 21:09:57 +0200

    Ejercicios 19,20,21

--- src/estructurasDeControl/Ejercicio19.java
+++ src/estructurasDeControl/Ejercicio19.java
@@ -1,5 +1,7 @@
 package estructurasDeControl;
 
+import java.util.Scanner;
+
 public class Ejercicio19 {
     public static void main(String[] args) {
         /*
@@ -8,9 +10,37 @@ semana correspondiente.
          */
 
         // Pido un número entero por teclado (1-7)
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce un número del 1 al 7:");
+        int num = sc.nextInt();
 
         // Visualizo el día de la semana
         // si 1 --> "Lunes"
         // si 2 --> "Martes"
+        switch (num) {
+            case 1:
+                System.out.println("Lunes");
+                break;
+            case 2:
+                System.out.println("Martes");
+                break;
+            case 3:
+                System.out.println("Miércoles");
+                break;
+            case 4:
+                System.out.println("Jueves");
+                break;
+            case 5:
+                System.out.println("Viernes");
+                break;
+            case 6:
+                System.out.println("Sábado");
+                break;
+            case 7:
+                System.out.println("Domingo");
+                break;
+            default:
+                System.out.println("El valor introducido no es correcto");
+        }
     }
 }
--- src/estructurasDeControl/Ejercicio21.java
+++ src/estructurasDeControl/Ejercicio21.java
@@ -1,5 +1,7 @@
 package estructurasDeControl;
 
+import java.util.Scanner;
+
 public class Ejercicio21 {
     public static void main(String[] args) {
         /*
@@ -16,8 +18,33 @@ por pantalla «no tenemos este topping, lo sentimos. » y a continuación inform
 del precio del helado sin ningún topping.
 Finalmente, el programa escribe por pantalla el precio del helado con el topping
 seleccionado (o ninguno).
-         */
+   */
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Menú:\n1. Sin topping\n2. Topping oreo\n3. KitKat\n4. Brownie\n5. Lacasitos");
+       int topping = sc.nextInt();
 
+       // Precio del helado sin topping
+        double precio = 1.9;
+
+       switch (topping) {
+           case 1:
+               System.out.println("El helado sin topping cuesta 1.90€. Precio: " + precio);
+               break;
+           case 2:
+               System.out.println("El topping de oreo cuesta 1€. Precio total: " + (precio + 1));
+               break;
+           case 3:
+               System.out.println("El topping de KitKat cuesta 1.50€. Precio total: " + (precio + 1.5));
+               break;
+           case 4:
+               System.out.println("El topping de brownie cuesta 0.75€. Precio total: " + (precio + 0.75));
+               break;
+           case 5:
+               System.out.println("El topping de lacasitos cuesta 0.95€. Precio total: " + (precio + 0.95));
+           default:
+               System.out.println("No tenemos este topping, lo sentimos");
+               System.out.println("El helado sin topping cuesta 1.90€. Precio: " + precio);
+       }
 
     }
 }
