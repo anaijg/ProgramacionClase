e0acb85d831a452282158891ec3fd8d54e339d27
anaijg <ana.jimenezgomez@educa.madrid.org>
2021-10-27 19:23:29 +0200

    Ejemplos y ejercicios while 27 octubre

---
+++ src/estructurasDeControl/Ejercicio38.java
@@ -0,0 +1,10 @@
+package estructurasDeControl;
+
+public class Ejercicio38 {
+    public static void main(String[] args) {
+        /*
+        38. Escribe un programa que pida números hasta que se introduzca un cero. Debe
+imprimir la suma y la media de todos los números introducidos.
+         */
+    }
+}
