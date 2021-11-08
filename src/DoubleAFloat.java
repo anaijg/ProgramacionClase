f3c52bd908991d0dd621c38c2af441d9380311bb
anaijg <71371140+anaijg@users.noreply.github.com>
2021-11-08 21:28:52 +0100

    Delete src directory

--- src/DoubleAFloat.java
+++
@@ -1,8 +0,0 @@
-public class DoubleAFloat {
-    public static void main(String[] args) {
-        long d = 1_000_000_000;
-        float fl = d;
-        System.out.println(fl);
-
-    }
-}
--- src/estructurasDeControl/Prueba2.java
+++
@@ -1,9 +0,0 @@
-package estructurasDeControl;
-
-public class Prueba2 {
-    public static void main(String[] args) {
-        int n = 35;
-
-
-    }
-}
